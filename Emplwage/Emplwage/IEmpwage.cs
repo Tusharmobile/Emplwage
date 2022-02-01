62cdf5d45e4846d60970ba2e954392f8b4a55458
tushar <tusharshitre123@gmail.com>
2022-02-01 18:10:27 +0530

    store total wage

---
+++ Emplwage/Emplwage/IEmpwage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emplwage
+{
+    internal interface IEmpwage
+    {
+        //Interface
+
+
+        void AddCompany(string company, int wagePrHrs, int totalWorkHrs, int totalWorkDay);
+        void GetWage();
+
+    }
+}
+    
+
